f69045043321c5dfcf17b9d190fb1eca66b01a5d
Adyasha8105 <adyashamohanty8105@gmail.com>
2022-01-02 17:26:15 +0530

    :art: Add try other product section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Features from "./components/Features";
 import Header from "./components/Header";
+import OtherProducts from "./components/OtherProducts";
 import SloganMaker from "./components/SloganMaker";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Header />
       <SloganMaker />
       <Features />
+      <OtherProducts />
     </div>
   );
 };
---
+++ src/components/OtherProducts.js
@@ -0,0 +1,52 @@
+import PrivacyImage from "../images/privacy.svg";
+import TermsConditionsImage from "../images/terms-conditions.svg";
+import DomainNameImage from "../images/domain-name.svg";
+import InvoiceImage from "../images/invoice.svg";
+
+const productsObj = [
+  {
+    title: "Privacy Policy Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+    image: PrivacyImage,
+  },
+  {
+    title: "Terms & Conditions Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+    image: TermsConditionsImage,
+  },
+  {
+    title: "Domain Name Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+    image: DomainNameImage,
+  },
+  {
+    title: "Invoice Generator",
+    description:
+      "Stock your store with 100s of products and start selling to customers in minutes, without the hassle of inventory or packaging.",
+    image: InvoiceImage,
+  },
+];
+
+const OtherProducts = () => {
+  return (
+    <div className="px-4 py-12">
+      <div className="w-[min(1024px,_100%)] mx-auto p-4 flex flex-col items-stretch justify-start gap-8">
+        <h2 className="flex font-semibold text-3xl justify-center sm:justify-center lg:justify-start">Try our other free products</h2>
+        <div className="grid grid-rows-none place-items-center gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {productsObj.map((product) => (
+            <div className="max-w-[300px] flex flex-col items-start justify-start gap-1" key={product.title}>
+              <img src={product.image} alt="logo" />
+              <p className="font-medium text-lg mt-2">{product.title}</p>
+              <p className="text-[hsl(0,_0%,_30%)]">{product.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OtherProducts;
