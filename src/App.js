beb2139df2dc467c0cb5926a62316597748b6f4d
Adyasha8105 <adyashamohanty8105@gmail.com>
2022-01-02 16:14:30 +0530

    :package: Create react app and incorporate tailwind

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="bg-white font-Galano">
+      Heyy
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: "#146EB4",
+      white: "#fff",
+      black: "#000",
+    },
+    extend: {
+      fontFamily: {
+        Galano: ["Galano Grotesque"],
+      },
+    },
+  },
+  plugins: [],
+};
